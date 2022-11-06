5439e4b71c065be305b550fef5742b4cc9c07948
drinkingWater <61174663+DrinkingWater64@users.noreply.github.com>
2022-11-06 22:18:32 +0600

    created a rectamgle with two triangles

--- Window.cpp
+++ Window.cpp
@@ -103,25 +103,34 @@ int main() {
 
 	// set up vertex data 9 and buffers and configure vertex attributes
 	
-	GLfloat vertices[] =
-	{
-		-0.9f, -0.5f, 0.0f,  // left 
-		-0.0f, -0.5f, 0.0f,  // right
-		-0.45f, 0.5f, 0.0f  // top 
+	GLfloat vertices[] = {
+			 0.5f,  0.5f, 0.0f,  // top right
+			 0.5f, -0.5f, 0.0f,  // bottom right
+			-0.5f, -0.5f, 0.0f,  // bottom left
+			-0.5f,  0.5f, 0.0f   // top left 
 	};
-
+	GLuint indices[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
 	// creating buffer objects
 	GLuint VAO;
 	GLuint VBO;
+	GLuint EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	// bind the vertex array first, then bind and set vertex buffer(s),
 	// and then configure vertex atrributes
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -149,8 +158,8 @@ int main() {
 		// draw triangle
 		glUseProgram(shaderprogram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 		//swap buffers and poll events
